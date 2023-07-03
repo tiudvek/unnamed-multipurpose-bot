4349a2fa5a0da0e86728ee92ced2a1efd7ef203d
tiudvek <tiudvek@gmail.com>
2023-07-02 20:54:26 -0400

    Add pyproject.toml, format again
    Only reason I did this was because requirements.txt was bein formatted by black for some reason :v

--- src/__main__.py
+++ src/__main__.py
@@ -1,5 +1,5 @@
 from src.core.bot import Bot
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     bot = Bot()
     bot.main()
--- src/core/bot.py
+++ src/core/bot.py
@@ -2,25 +2,25 @@ import json
 import os
 from disnake.ext import commands
 
-config = json.load(open('src/core/config.json'))
+config = json.load(open("src/core/config.json"))
 
 
 class Bot(commands.InteractionBot):
     def __init__(self):
         self.config = config
-        super().__init__(test_guilds=config['bot']['test_guilds'])
+        super().__init__(test_guilds=config["bot"]["test_guilds"])
 
     async def on_ready(self):
-        print(f'Ready on {self.user} ({self.user.id})')
+        print(f"Ready on {self.user} ({self.user.id})")
 
     def load_extensions(self):
-        for filename in os.listdir('src/ext'):
-            if filename.endswith('.py'):
+        for filename in os.listdir("src/ext"):
+            if filename.endswith(".py"):
                 cog_name = filename[:-3]  # Remove the '.py' extension
-                cog_path = f'src.ext.{cog_name}'
+                cog_path = f"src.ext.{cog_name}"
                 self.load_extension(cog_path)
-                print(f'Loaded extension: {cog_path}')
+                print(f"Loaded extension: {cog_path}")
 
     def main(self):
         self.load_extensions()
-        self.run(self.config['bot']['token'])
+        self.run(self.config["bot"]["token"])
